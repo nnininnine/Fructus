1e21b0277d157d9174c9fdb686b6d0a4a7c66784
Nuttapon Buaban <nuttapon.dev@gmail.com>
2022-07-12 15:36:20 +0700

    feat(onboarding): create OnboardingView

---
+++ Fructus/Screens/Onboarding/View/OnboardingView.swift
@@ -0,0 +1,20 @@
+//
+//  OnboardingView.swift
+//  Fructus
+//
+//  Created by 7Peaks on 12/7/2565 BE.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct OnboardingView_Previews: PreviewProvider {
+    static var previews: some View {
+        OnboardingView()
+    }
+}
