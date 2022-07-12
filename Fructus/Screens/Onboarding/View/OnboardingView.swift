74e94b45af5dc9713735f1bf691d0f23d16af8d8
Nuttapon Buaban <nuttapon.dev@gmail.com>
2022-07-12 16:15:03 +0700

    feat(onboarding): implement OnboardingView with FruitCardView

--- Fructus/Screens/Onboarding/View/OnboardingView.swift
+++ Fructus/Screens/Onboarding/View/OnboardingView.swift
@@ -8,13 +8,24 @@
 import SwiftUI
 
 struct OnboardingView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+  // MARK: Properties
+
+  // MARK: Body
+
+  var body: some View {
+    TabView {
+      ForEach(0 ..< 5) { _ in
+        FruitCardView()
+          .padding(.horizontal)
+      } //: ForEach
+    } //: TabView
+    .tabViewStyle(.page)
+    .padding(.vertical, 20)
+  }
 }
 
 struct OnboardingView_Previews: PreviewProvider {
-    static var previews: some View {
-        OnboardingView()
-    }
+  static var previews: some View {
+    OnboardingView()
+  }
 }
