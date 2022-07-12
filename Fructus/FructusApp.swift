60c08d11f1671775b3cba3afb03407934a3ba9af
Nuttapon Buaban <nuttapon.dev@gmail.com>
2022-07-12 14:13:40 +0700

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by 7Peaks on 12/7/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
