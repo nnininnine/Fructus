aa416295536df354b56c0aa8b0e8fd3cab944031
Nuttapon Buaban <nuttapon.dev@gmail.com>
2022-07-12 15:00:36 +0700

    feat: create FruitCardView

---
+++ Fructus/Screens/Onboarding/View/FruitCardView.swift
@@ -0,0 +1,20 @@
+//
+//  FruitCardView.swift
+//  Fructus
+//
+//  Created by 7Peaks on 12/7/2565 BE.
+//
+
+import SwiftUI
+
+struct FruitCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FruitCardView_Previews: PreviewProvider {
+    static var previews: some View {
+        FruitCardView()
+    }
+}
