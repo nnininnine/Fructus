241a243cb35dd8a72826f1a39883de603ea314f6
Nuttapon Buaban <nuttapon.dev@gmail.com>
2022-07-12 15:20:49 +0700

    feat(onboarding): implement FruitCardView

--- Fructus/Screens/Onboarding/View/FruitCardView.swift
+++ Fructus/Screens/Onboarding/View/FruitCardView.swift
@@ -8,13 +8,40 @@
 import SwiftUI
 
 struct FruitCardView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+  var body: some View {
+    VStack(spacing: 20) {
+      Spacer()
+      // Image
+      Image("blueberry")
+        .resizable()
+        .scaledToFit()
+        .shadow(color: .black.opacity(0.15), radius: 8, x: 6, y: 8)
+      // Title
+      Text("Blueberry")
+        .foregroundColor(.white)
+        .font(.largeTitle)
+        .fontWeight(.heavy)
+        .shadow(color: .black.opacity(0.15), radius: 2, x: 2, y: 2)
+
+      // Headline
+      Text("Blueberries are sweet, nutritious and wildly popular fruit all over the world.")
+        .foregroundColor(.white)
+        .multilineTextAlignment(.center)
+        .padding(.horizontal, 16)
+        .frame(maxWidth: 480)
+
+      // Start button
+
+      Spacer()
+    } //: VStack
+    .background(LinearGradient(gradient: Gradient(colors: [Color("ColorBlueberryLight"), Color("ColorBlueberryDark")]), startPoint: .top, endPoint: .bottom))
+    .cornerRadius(20)
+  }
 }
 
 struct FruitCardView_Previews: PreviewProvider {
-    static var previews: some View {
-        FruitCardView()
-    }
+  static var previews: some View {
+    FruitCardView()
+      .previewLayout(.fixed(width: 320, height: 640))
+  }
 }
