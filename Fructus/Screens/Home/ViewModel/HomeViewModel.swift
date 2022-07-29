55be5cf394c593d3d96ae757ad802131a210566b
Nuttapon Buaban <nuttapon.dev@gmail.com>
2022-07-29 11:48:43 +0700

    feat(home): create HomeViewModel

---
+++ Fructus/Screens/Home/ViewModel/HomeViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  HomeViewModel.swift
+//  Fructus
+//
+//  Created by 7Peaks on 29/7/2565 BE.
+//
+
+import SwiftUI
+
+class HomeViewModel: ObservableObject {
+  // MARK: - Properties
+
+  // MARK: - Init
+
+  // MARK: - Methods
+}
