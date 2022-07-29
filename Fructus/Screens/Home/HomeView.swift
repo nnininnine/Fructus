c35856109e5757e8d7517214e17ad2ccb4070539
Nuttapon Buaban <nuttapon.dev@gmail.com>
2022-07-29 11:34:07 +0700

    feat(home): implement FruitRowView in HomeView

--- Fructus/Screens/Home/HomeView.swift
+++ Fructus/Screens/Home/HomeView.swift
@@ -8,14 +8,28 @@
 import SwiftUI
 
 struct HomeView: View {
+  // MARK: - Properties
+
+  var fruits: Fruits = fruitsData
+
+  // MARK: - Body
+
   var body: some View {
-    Text("Hello, world!")
-      .padding()
+    NavigationView {
+      List {
+        ForEach(fruits.shuffled()) { item in
+          FruitRowView(fruit: item)
+            .padding(.vertical, 8)
+        }
+      } //: List
+      .listStyle(.inset)
+      .navigationTitle("Fruits")
+    } //: NavigationView
   }
 }
 
 struct ContentView_Previews: PreviewProvider {
   static var previews: some View {
-    HomeView()
+    HomeView(fruits: fruitsData)
   }
 }
