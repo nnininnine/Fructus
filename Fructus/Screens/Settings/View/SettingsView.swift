568bc39b03c63b8d550ed1089fdd96b27389cf06
Nuttapon Buaban <nuttapon.dev@gmail.com>
2022-08-01 15:32:32 +0700

    refactor: format, add mark in SettingsView

--- Fructus/Screens/Settings/View/SettingsView.swift
+++ Fructus/Screens/Settings/View/SettingsView.swift
@@ -8,13 +8,17 @@
 import SwiftUI
 
 struct SettingsView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+  // MARK: - Properties
+
+  // MARK: - Body
+
+  var body: some View {
+    Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  }
 }
 
 struct SettingsView_Previews: PreviewProvider {
-    static var previews: some View {
-        SettingsView()
-    }
+  static var previews: some View {
+    SettingsView()
+  }
 }
