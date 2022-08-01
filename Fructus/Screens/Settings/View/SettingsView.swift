2719a220cc4e3606ed2e47b3683b652123ca8d3e
Nuttapon Buaban <nuttapon.dev@gmail.com>
2022-08-01 14:57:50 +0700

    feat: create SettingsView

---
+++ Fructus/Screens/Settings/View/SettingsView.swift
@@ -0,0 +1,20 @@
+//
+//  SettingsView.swift
+//  Fructus
+//
+//  Created by 7Peaks on 1/8/2565 BE.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsView()
+    }
+}
